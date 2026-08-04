18fc3b3db220b0758664be7ffcd26633c740f1df
Jake <jab7910@utulsa.edu>
2026-08-04 15:52:30 -0500

    Rework sponsor award winner selection

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1470,8 +1470,11 @@ func Routes(app *config.AppContext) (http.Handler, error) {
 	r.HandleFunc("/{conf}/hackathon/judging/scorecards", func(w http.ResponseWriter, r *http.Request) {
 		HackathonScorecardSubmit(w, r, app)
 	}).Methods("POST")
-	r.HandleFunc("/{conf}/hackathon/judging/award-votes", func(w http.ResponseWriter, r *http.Request) {
-		HackathonAwardVoteSubmit(w, r, app)
+	r.HandleFunc("/{conf}/hackathon/judging/award-winners", func(w http.ResponseWriter, r *http.Request) {
+		HackathonAwardWinnerAssign(w, r, app)
+	}).Methods("POST")
+	r.HandleFunc("/{conf}/hackathon/judging/award-winners/remove", func(w http.ResponseWriter, r *http.Request) {
+		HackathonAwardWinnerRemove(w, r, app)
 	}).Methods("POST")
 	r.HandleFunc("/{conf}/hackathon/projects/new", func(w http.ResponseWriter, r *http.Request) {
 		HackathonProjectNew(w, r, app)
